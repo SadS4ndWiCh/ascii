a809ac0f6ea12f2030c17e885aac8f21bcec45e0
sads4ndwich <caiocamargo007@gmail.com>
2024-07-15 18:16:38 -0300

    feat: convert image to ascii

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/SadS4ndWiCh/ascii
 
 go 1.22.4
+
+require github.com/sunshineplan/imgconv v1.1.10
+
+require (
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pdfcpu/pdfcpu v0.8.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sunshineplan/pdf v1.0.7 // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/sunshineplan/imgconv"
+	"golang.org/x/term"
+)
+
+const CHARACTERS = " .:coPO?@■"
+const CHARACTERS_LENGTH = len(CHARACTERS)
+
+func rgbtogray(r, g, b float64) float64 {
+	return 0.3*r + 0.59*g + 0.11*b
+}
+
+func imageToAscii(src string, w int, h int) (string, error) {
+	img, err := imgconv.Open(src)
+	if err != nil {
+		return "", err
+	}
+
+	buf := strings.Builder{}
+	img = imgconv.Resize(img, &imgconv.ResizeOption{Width: w, Height: h})
+	bounds := img.Bounds()
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			r = r % 256
+			g = g % 256
+			b = b % 256
+
+			intensity := rgbtogray(float64(r), float64(g), float64(b))
+			idx := int(intensity / 255 * float64(CHARACTERS_LENGTH-1))
+
+			buf.WriteByte(CHARACTERS[idx])
+			buf.WriteByte(CHARACTERS[idx])
+			buf.WriteByte(CHARACTERS[idx])
+		}
+
+		buf.WriteByte('\n')
+	}
+
+	return buf.String(), nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	defaultWidth, defaultHeight, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		defaultWidth = 166
+		defaultHeight = 37
+	}
+
+	input := flag.String("i", "./input.jpg", "The input file")
+	width := flag.Int("w", defaultWidth, "Set a custom width")
+	height := flag.Int("h", defaultHeight, "Set a custom height")
+	square := flag.Bool("square", false, "Use square aspect ratio")
+
+	flag.Parse()
+
+	if *square {
+		*width = *height
+	}
+
+	ext := strings.Split(*input, ".")[1]
+
+	switch ext {
+	case "jpg", "jpeg", "png":
+		buf, err := imageToAscii(*input, *width, *height)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(buf)
+	default:
+		fmt.Printf("[-] Invalid format: .%s\n", ext)
+	}
 }
